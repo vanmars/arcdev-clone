730431e3b7cb76b770a2a23c6a8b157c4e956ccd
Vanessa Stewart <vstewart1213@gmail.com>
2021-01-08 14:20:51 -0800

    Add Header with navigation

---
+++ src/components/ui/Header.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import logo from '../../assets/logo.svg';
+import Button from '@material-ui/core/Button';
+
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+};
+
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em"
+  },
+  logo: {
+    height: "7em"
+  },
+  tabContainer: {
+    marginLeft: "auto"
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px"
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginRight: "25px",
+    marginLeft: "50px",
+    height: "45px"
+  }
+}))
+
+const Header = (props) => {
+  const classes = useStyles();
+
+  return ( 
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position='fixed'>
+          <Toolbar disableGutters>
+            <img src={logo} alt="company logo" className={classes.logo} />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
+            <Button variant="contained" color="secondary" className={classes.button}>
+              Free Estimate
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin}></div>
+    </React.Fragment>
+   );
+}
+ 
+export default Header;
