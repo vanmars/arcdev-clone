4032c31a73a63c9926ba3addee02af0171d588ed
Vanessa Stewart <vstewart1213@gmail.com>
2021-01-12 14:12:13 -0800

    Add and style contact and about us section on splash page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -14,6 +14,7 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import revolutionBackground from '../assets/repeatingBackground.svg';
+import infoBackground from '../assets/infoBackground.svg';
 // import Card from '@material-ui/core/Card;'
 // import Card from '@material-ui/core/Card;'
 
@@ -109,13 +110,22 @@ const useStyles = makeStyles(theme => ({
       borderRadius: 0,
       wdith: "100%"
     }
+  },
+  infoBackground: {
+    backgroundImage: `url(${infoBackground})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%"
   }
 }))
 
 const LandingPage = () => {
   const classes = useStyles();
   const theme = useTheme();
-  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"))
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
 
   const defaultOptions = {
     loop: true,
@@ -233,7 +243,7 @@ const LandingPage = () => {
                     <Typography variant="subtitle1" gutterBottom>
                       Visionary insights couples with cutting-edget technology is a recipe for revolution.
                     </Typography>
-                    <Button variant="outlined" className={classes.learnButtonHero}>
+                    <Button variant="outlined" className={classes.learnButton}>
                       Learn More
                     </Button>
                   </Grid>
@@ -245,9 +255,56 @@ const LandingPage = () => {
           </Grid>
         </Grid>
 
+        <Grid item> {/*--------- INFORMATION BLOCK---------- */}
+          <Grid container direction="row" alignItems="center" style={{height: "80em"}}>
+  
+            <Grid item container 
+              style={{position: "absolute", textAlign: matchesXS ? "center" : "inherit"}} 
+              direction={matchesXS ? "column" : "row"}
+              spacing={matchesXS ? 10 : 0}
+            >
+
+              <Grid item sm style={{marginLeft: matchesXS ? 0 : matchesSM ? "2em" : "5em"}}>
+                <Grid container direction="column">
+                  <Typography variant="h2" style={{color: "white"}}>
+                    About Us
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    Let's get personal.
+                  </Typography>
+                  <Grid item>
+                    <Button variant="outlined" className={classes.learnButton} style={{color: "white", borderColor: "white"}}>
+                      Learn More
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+
+              <Grid item sm style={{marginRight: matchesXS ? 0 : matchesSM ? "2em" : "5em", textAlign: matchesXS ? "center" : "right"}}>
+                <Grid container direction="column">
+                  <Typography variant="h2" style={{color: "white"}}>
+                    Contact Us
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    Say hello! <span frole="img" aria-label="waving hand">👋</span>
+                  </Typography>
+                  <Grid item>
+                    <Button variant="outlined" className={classes.learnButton} style={{color: "white", borderColor: "white"}}>
+                      Learn More
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+
+            </Grid>
+
+            <div className={classes.infoBackground} />
+
+          </Grid>
+        </Grid>
+
       </Grid>
-      
-      </React.Fragment>
+    </React.Fragment>
    );
 }
  
