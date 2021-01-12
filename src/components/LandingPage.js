ce4947f5da460d16cebadd46db255f2d5b29eb45
Vanessa Stewart <vstewart1213@gmail.com>
2021-01-12 11:32:13 -0800

    Finish responsive styling of hero block on splash page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,23 +1,71 @@
 import React from 'react';
 import Lottie from 'react-lottie';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import ButtonArrow from '../components/ui/ButtonArrow';
 import animationData from '../animations/landinganimation/data'
 import Typography from '@material-ui/core/Typography';
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 
 const useStyles = makeStyles(theme => ({
   animation: {
     maxWidth: "50em",
     minWidth: "21em",
     marginTop: "2em",
-    marginLeft: "10%"
+    marginLeft: "10%",
+    [theme.breakpoints.down("sm")]: {
+      maxWidth: "30em"
+    }
+  },
+  estimateButton: {
+    ...theme.typography.estimate,
+    backgroundColor: theme.palette.common.orange,
+    borderRadius: 50,
+    height: 45,
+    width: 145,
+    marginRight: 40,
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.light
+    }
+  },
+  buttonContainer: {
+    marginTop: "1em"
+  },
+  learnButtonHero: {
+    borderColor: theme.palette.common.blue,
+    color: theme.palette.common.blue,
+    borderWidth: 2,
+    testTransform: "none",
+    borderRadius: 50,
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    fontSize: "0.9rem",
+    height: 45,
+    width: 145
+
+  },
+  mainContainer: {
+    marginTop: "5em",
+    [theme.breakpoints.down("md")]: {
+      marginTop: "3em"
+    },
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "2em"
+    }
+  },
+  heroTextContainer: {
+    minWidth: "21.5em",
+    marginLeft: "1em",
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: 0
+    }
   }
 }))
 
 const LandingPage = () => {
   const classes = useStyles();
+  const theme = useTheme();
 
   const defaultOptions = {
     loop: true,
@@ -30,24 +78,23 @@ const LandingPage = () => {
 
   return ( 
     <React.Fragment>
-      <Grid container direction="column">
+      <Grid container direction="column" className={classes.mainContainer}>
         <Grid item>
 
           <Grid container direction="row" justify="flex-end" alignItems="center">
-            <Grid sm item>
+            <Grid sm item clasName={classes.heroTextContainer}>
               <Typography variant="h2" align="center">Bring West Coast Technology<br />to the Midwest</Typography>
-              <Grid container>
+              <Grid container justify="center" className={classes.buttonContainer}>
 
                 <Grid item>
-                  <Button variant="contained">
+                  <Button className={classes.estimateButton} variant="contained">
                     Free Estimate
                   </Button>
                 </Grid>
 
                 <Grid item>
-                  <Button variant="outlined">
+                  <Button variant="outlined" className={classes.learnButtonHero}>
                     Learn More
-                    <ButtonArrow width={15} height={15} fill="red" />
                   </Button>
                 </Grid>
 
